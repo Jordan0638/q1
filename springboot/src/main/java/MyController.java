a45236da6bd03df57441ccb195c25602c05d998d
Jordan0638 <jordan.shaju2023@vitstudent.ac.in>
2026-04-16 15:45:13 +0530

    Springboot

---
+++ springboot/src/main/java/MyController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot CI/CD working";
+    }
+}
---
+++ springboot/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+        // If Spring fails to start → test FAILS
+        assertTrue(true);
+    }
+}
